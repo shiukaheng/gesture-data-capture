00c9f1fe51367a1639612e8b4cf85dc8f3276685
Heng <31088159+shiukaheng@users.noreply.github.com>
2021-10-22 19:05:06 +0800

    Create InteractiveElements.js

---
+++ src/lib/InteractiveElements.js
@@ -0,0 +1,6 @@
+function samplePointInFrustum(horizontal_fov, vertical_fov, max_distance) {
+    const horizontal_deg = Math.random() * horizontal_fov - horizontal_fov / 2
+    const vertical_deg = Math.random() * vertical_fov - vertical_fov / 2
+    const distance = Math.random() * max_distance
+    return 
+}
