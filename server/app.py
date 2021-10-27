761cbb79a85bc26249126a990318f589bac79961
Heng <31088159+shiukaheng@users.noreply.github.com>
2021-10-27 19:47:37 +0800

    experimenting with backend

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+application = Flask(__name__)
+api = Api(application,
+          version='0.1',
+          title='Our sample API',
+          description='This is our sample API',
+)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    application.run(debug=True)
